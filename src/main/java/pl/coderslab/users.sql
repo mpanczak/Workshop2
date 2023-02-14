2d6cce294a56845c579193881eb66c16cca2889d
mpanczak <panczakmateusz@gmail.com>
2023-02-14 15:11:39 +0100

    database create

---
+++ src/main/java/pl/coderslab/users.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE workshop2
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE workshop2;
+
+DROP TABLE users;
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+DESCRIBE users;
