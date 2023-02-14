baf2b36f58913317e1a476180300c9f991d84a10
mpanczak <panczakmateusz@gmail.com>
2023-02-14 15:58:38 +0100

    User and UserDAO created

---
+++ src/main/java/pl/coderslab/entity/UserDAO.java
@@ -0,0 +1,14 @@
+package pl.coderslab.entity;
+
+public class UserDAO {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    private static final String READ_USER_QUERY =
+            "SELECT * FROM users WHERE id = ?";
+    private static final String UPDATE_USER_QUERY =
+            "UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?";
+    private static final String DELETE_USER_QUERY =
+            "DELETE FROM users WHERE id = ?";
+
+
+}
